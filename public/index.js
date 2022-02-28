fbad9e25f4cf820e62d6c1783cbfa57d5f7c3417
joykhera <73572578+joykhera@users.noreply.github.com>
2022-02-28 00:06:03 -0500

    a

--- public/index.js
+++ public/index.js
@@ -1,44 +1,46 @@
-const ws = new WebSocket(`${location.protocol == 'http:' ? 'ws' : 'wss'}://${location.host + location.pathname}`)
-let iframe, currentVideoId, playlistId
+let iframe, currentVideo, playlistId, musicLoaded
 
-const params = Object.fromEntries(new URLSearchParams(window.location.search).entries())
-ws.addEventListener('open', () => {
-    if (params.playlistId) ws.send(JSON.stringify({ state: 'music', playlistId: params.playlistId }))
-})
-
-ws.addEventListener('message', (data) => {
-    const msg = JSON.parse(data.data)
-
-    if (msg.state == 'music not ready') {
-        if (!document.getElementById(msg.state)) {
-            const p = document.createElement("p")
-            p.setAttribute('id', msg.state)
-            p.innerHTML = msg.message
-            document.body.appendChild(p)
-        }
-    }
-
-    else if (msg.state == 'join music') {
-        currentVideoId = msg.currentVideoId
-        iframe = document.createElement("iframe")
-        iframe.setAttribute('id', 'iframe')
-        iframe.setAttribute('src', `https://www.youtube.com/embed/${currentVideoId}?autoplay=1&start=${msg.currentVideoTimeElapsed}`)
-        iframe.setAttribute('allow', 'autoplay')
-        iframe.setAttribute('allow', 'fullscreen')
-        document.body.appendChild(iframe)
-        if (document.getElementById('music not ready')) document.getElementById('music not ready').remove()
-        document.getElementById('input').style.display = 'none'
-    }
-
-    else if (msg.state == 'new music') {
-        currentVideoId = msg.currentVideoId
-        if (iframe) iframe.src = `https://www.youtube.com/embed/${currentVideoId}?autoplay=1`
-    }
-})
-
-document.getElementById('playlistId').onkeypress = function (e) {
-    playlistId = this.value.includes("https://www.youtube.com/playlist?list=") ? this.value.slice(38) : this.value
-    if ((e.code == 'Enter' || e.key == 'Enter') && ws.readyState == 1) ws.send(JSON.stringify({ state: 'music', playlistId }))
+function createIframe() {
+    iframe = document.createElement("iframe")
+    iframe.setAttribute('id', 'iframe')
+    iframe.setAttribute('src', `https://www.youtube.com/embed/${currentVideo.id}?autoplay=1&start=${Math.floor((Date.now() - videoStartTime) / 1000)}`)
+    iframe.setAttribute('allow', 'autoplay')
+    iframe.setAttribute('allow', 'fullscreen')
+    document.body.appendChild(iframe)
+    if (document.getElementById('music not ready')) document.getElementById('music not ready').remove()
+    document.getElementById('input').style.display = 'none'
 }
 
-ws.addEventListener('close', () => window.open(location.href + '?playlistId=' + playlistId, "_self"))
+document.getElementById('playlistId').onkeydown = function (e) {
+    playlistId = this.value.includes("https://www.youtube.com/playlist?list=") ? this.value.slice(38) : this.value
+    if (e.code == 'Enter' || e.key == 'Enter') loadMusic(playlistId)
+}
+
+const videos = []
+let nextPageToken = true
+async function loadMusic(playlistId) {
+    const p = document.createElement("p")
+    p.textContent = "loading"
+    document.body.appendChild(p)
+    while (nextPageToken) {
+        const playlistBody = await (typeof nextPageToken == 'string' ?
+            await fetch(`https://www.googleapis.com/youtube/v3/playlistItems?pageToken=${nextPageToken}&part=contentDetails&key=AIzaSyB6f1sSrQtaLlJlQgBrO5MKmm_eaX_Qzxs&maxResults=50&playlistId=${playlistId}`) :
+            await fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&key=AIzaSyB6f1sSrQtaLlJlQgBrO5MKmm_eaX_Qzxs&maxResults=50&playlistId=${playlistId}`)).json()
+        const videosBody = await (await fetch(`https://www.googleapis.com/youtube/v3/videos?part=contentDetails&key=AIzaSyB6f1sSrQtaLlJlQgBrO5MKmm_eaX_Qzxs&id=${playlistBody.items.map((video) => video.contentDetails.videoId).join(',')}`)).json()
+        for (const video of videosBody.items) videos.push(video)
+        nextPageToken = playlistBody.nextPageToken
+    }
+    musicLoaded = true
+    p.remove()
+    simulateMusic()
+    createIframe()
+}
+
+let videoStartTime;
+function simulateMusic() {
+    currentVideo = videos[Math.floor(Math.random() * videos.length)]
+    videoStartTime = Date.now()
+    currentVideo.duration = moment.duration(currentVideo.contentDetails.duration)._milliseconds
+    if (iframe) iframe.src = `https://www.youtube.com/embed/${currentVideo.id}?autoplay=1`
+    setTimeout(simulateMusic, currentVideo.duration);
+}
